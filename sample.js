73e1b521c648ec46d59016bd0708712d94ade13e
msakamoto1102 <79570154+msakamoto1102@users.noreply.github.com>
2023-03-16 14:39:46 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -2,6 +2,7 @@ function header(){
     $.ajax({
         url: "header.html",
         cache: false,
+        async: true,
         success: function(html){
             document.write(html);
         }
