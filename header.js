6b4c00099197111f63e8199927db93c15b465740
msakamoto1102 <79570154+msakamoto1102@users.noreply.github.com>
2023-03-17 18:46:44 +0900

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ function header(rootDir){
     $.ajax({
         url: rootDir + "header.html",
         cache: false,
-        async: false,
+        async: true,
         success: function(html){
             html = html.replace(/\{root\}/g, rootDir);
             document.write(html);
